865ef99c7d6bca3fa4d73a29120175a99b057025
Humberto BM <89049194+Kooppa@users.noreply.github.com>
2022-09-24 13:18:20 -0500

    first

---
+++ HashaBank.Presentation/HashaBank.Presentation/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main()
+    {
+        System.Console.WriteLine("******Harsha Bank******");
+        System.Console.WriteLine("Login Page");
+
+        string userName = null, password = null;
+
+        System.Console.WriteLine("Username: ");
+        userName = System.Console.ReadLine();
+
+        if (userName != "")
+        {
+            System.Console.WriteLine("Password: ");
+            password = System.Console.ReadLine();
+        }
+
+        if (userName == "system" && password == "manager")
+        {
+            System.Console.WriteLine("TO DO: Main menu  here ");
+        }
+
+        System.Console.WriteLine("Thank you");
+        System.Console.ReadKey();
+
+    }
+}
